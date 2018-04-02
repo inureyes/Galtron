9755d32e67011c03369284be170ae55569c47064
jeongin <jil8885@hanyang.ac.kr>
2018-04-02 23:15:53 +0900

    fix scoreboard ui

--- scoreboard.py
+++ scoreboard.py
@@ -32,7 +32,7 @@ class Scoreboard():
 		#Display the score at the top left corner
 		self.scoreRect = self.scoreImg.get_rect()
 		self.scoreRect.right = self.screenRect.right - 20
-		self.scoreRect.top = 10
+		self.scoreRect.top = 30
 
 	def prepHighScore(self):
 		"""Turn the high score into a rendered image"""
@@ -43,8 +43,8 @@ class Scoreboard():
 			self.setting.bgColor)
 		#Center the highscore
 		self.highScoreRect = self.highScoreImg.get_rect()
-		self.highScoreRect.centerx = self.screenRect.centerx
-		self.highScoreRect.top = self.scoreRect.top
+		self.highScoreRect.right = self.screenRect.right - 20
+		self.highScoreRect.top = 10
 
 	def prepLevel(self):
 		"""Turn the level into a rendered image."""
