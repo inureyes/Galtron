f37c46de60e98832cadd46c3c0d292836ac36abc
Ramenseller <moon3545@naver.com>
2018-04-04 13:31:08 +0900

    new Galtron 2018.04.04.13.31

--- ship.py
+++ ship.py
@@ -45,54 +45,6 @@ class Ship(Sprite):
         self.timer2 = 0
         self.trajectory = 0
 
-	def update(self, bullets, aliens):
-		self.image = pg.image.load(checkColor())
-		"""Update the ships position"""
-		if self.movingRight and self.rect.right < self.screenRect.right:
-			self.center += self.setting.shipSpeed
-			self.image = pg.transform.rotate(self.image,-45)
-		if self.movingLeft and self.rect.left > 1:
-			self.center -= self.setting.shipSpeed
-			self.image = pg.transform.rotate(self.image,45)
-		if self.movingRight and self.rect.right >= self.screenRect.right:
-			self.center = 1.0
-		if self.movingLeft and self.rect.left <= 1:
-			self.center = self.screenRect.right
-		if self.movingUp and self.rect.top > self.screenRect.top + self.rect.height + 10:
-			self.centery -= self.setting.shipSpeed
-		if self.movingDown and self.rect.bottom < self.screenRect.bottom:
-			self.centery += self.setting.shipSpeed
-		if self.shoot == True:
-			if self.timer2 > 10:
-				self.image = pg.transform.rotate(self.image, 0)
-				if self.chargeGauge < 100:
-					self.chargeGauge += 2
-				else:
-					self.chargeGauge = 100
-				self.timer2 = 0
-			else:
-				self.timer2 += 1
-			if self.timer > 10 and len(bullets) < 10:
-				sounds.attack.play()
-				if(self.trajectory == 4):
-					newBullet0 = Bullet(self.setting, self.screen, self, 0)
-					newBullet1 = Bullet(self.setting, self.screen, self, 1)
-					newBullet2 = Bullet(self.setting, self.screen, self, 2)
-					bullets.add(newBullet0)
-					bullets.add(newBullet1)
-					bullets.add(newBullet2)
-				else:
-					newBullet = Bullet(self.setting, self.screen, self, self.trajectory)
-					bullets.add(newBullet)
-				sounds.attack.play()
-				self.timer = 0
-			else:
-				self.timer += 1
-		else:
-			if (self.chargeGauge == 100):
-				newBullet = Bullet(self.setting, self.screen, self, self.trajectory, 2)
-				bullets.add(newBullet)
-				self.chargeGauge = 0
         self.chargeGaugeStartTime = 0
         self.fullChargeTime = 2500
         self.chargeGauge = 0
