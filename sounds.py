9ee883adc2792a03e3af34d0cfbb2b922e8b65eb
jesin <audgus0914@naver.com>
2018-04-14 15:30:00 +0900

    no message

--- sounds.py
+++ sounds.py
@@ -22,7 +22,13 @@ heal_sound.set_volume(0.4)
 slowdown_sound = pg.mixer.Sound('sound_effects/slowdown.wav')
 slowdown_sound.set_volume(0.4)
 
-shield_sound = pg.mixer.Sound('sound_effects/shield.wav')
+<<<<<<< HEAD
+=======
+slow_sound = pg.mixer.Sound('sound_effects/slow_sound.wav')
+slow_sound.set_volume(0.4) 
+
+>>>>>>> 78ecfa2143bfd3ecfc66c1265fc3edf149138738
+shield_sound = pg.mixer.Sound('sound_effects/shield_sound.wav')
 shield_sound.set_volume(0.4)
 
 ult_attack = pg.mixer.Sound("sound_effects/ult_attack.wav")
@@ -46,5 +52,12 @@ explosion_sound = pg.mixer.Sound('sound_effects/explosion.wav')
 explosion_sound.set_volume(0.15)
 
 enemy_explosion_sound = pg.mixer.Sound('sound_effects/enemy_explosion.wav')
+<<<<<<< HEAD
+enemy_explosion_sound.set_volume(0.8)
+=======
 enemy_explosion_sound.set_volume(0.4)
+>>>>>>> 78ecfa2143bfd3ecfc66c1265fc3edf149138738
+
+
+
 
