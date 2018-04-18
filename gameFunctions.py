c614b9c5882d69c834a8bd2af46117772f734674
lee hyejin <37171669+bean3@users.noreply.github.com>
2018-04-19 08:53:16 +0900

    Update gameFunctions.py

--- gameFunctions.py
+++ gameFunctions.py
@@ -471,15 +471,6 @@ def checkBulletAlienCol(setting, screen, stats, sb, ship, aliens, bullets, eBull
                 if(i <= setting.probability):
                 	createItem(setting, screen, stats, alien.rect.x,alien.rect.y,random.
                 		randrange(1,5),items)
-                #if i<=setting.probabilityHeal:
-                #    createItem(setting, screen, stats, alien.rect.x, alien.rect.y, 1, items)
-                #if setting.probabilityHeal<i<=setting.probabilityHeal+setting.probabilityTime:
-                #    createItem(setting, screen, stats, alien.rect.x, alien.rect.y, 2, items)
-                #if setting.probabilityHeal+setting.probabilityTime<i<=setting.probabilityHeal+setting.probabilityTime+setting.probabilityShield:
-                #    createItem(setting, screen, stats, alien.rect.x, alien.rect.y, 3, items)
-                #if setting.probabilityHeal+setting.probabilityTime+setting.probabilityShield<i<=setting.probabilityHeal+setting.probabilityTime+setting.probabilityShield+setting.probabilitySpeed:
-                #    createItem(setting, screen, stats, alien.rect.x, alien.rect.y, 4, items)
-
                 aliens.remove(alien)
 
         # Increase the ultimate gauge, upto 100
