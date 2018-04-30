9217a1364738dbae003e7e2847d9617c9836e764
DESKTOP-KOOK\KOOK <9858momo@gmail.com>
2018-04-30 22:36:32 +0900

    add boss easter egg

--- gameFunctions.py
+++ gameFunctions.py
@@ -269,6 +269,8 @@ def createBoss(setting, stats, screen, aliens, alienNumber, rowNumber):
     alien.rect.x = setting.screenWidth / 2
     alien.rect.y = 80
     aliens.add(alien)
+    #boss easter egg
+    alien.image = pg.image.load('gfx/boss.png')
     boss = alien
 
 
